b774abbef9a6ab64e4872c15fbe42eccc07d1d95
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-05-02 09:49:44 +0900

    Not using namespace.

--- minisat.go.cpp
+++ minisat.go.cpp
@@ -2,32 +2,30 @@
 #include <minisat/core/SolverTypes.h>
 #include "minisat.go.h"
 
-using namespace Minisat;
-
 
 extern "C" WrapSolver NewSolver() {
-  return (WrapSolver) new Solver();
+  return (WrapSolver) new Minisat::Solver();
 }
 
 extern "C" WrapVar WrapSolverNewVar(WrapSolver slv) {
-    return ((Solver*) slv)->newVar();
+    return ((Minisat::Solver*) slv)->newVar();
 }
 
 extern "C" int WrapSolverSolve(WrapSolver slv) {
-  return ((Solver*) slv)->solve() ? 1 : 0;
+  return ((Minisat::Solver*) slv)->solve() ? 1 : 0;
 }
 
 extern "C" WrapLit WrapMkLit(WrapVar v, int sign) {
-  return mkLit(v, bool(sign)).x;
+  return Minisat::mkLit(v, bool(sign)).x;
 }
 
 extern "C" int WrapSolverAddClause(WrapSolver slv, WrapLit* lits, int len) {
- vec<Lit> vec_lits;
+  Minisat::vec<Minisat::Lit> vec_lits;
  
   for (int i = 0; i < len; i++) {
-    Lit l;
+    Minisat::Lit l;
     l.x = lits[i];
     vec_lits.push(l);
   }
-  return ((Solver*) slv)->addClause_(vec_lits) ? 1 : 0;
+  return ((Minisat::Solver*) slv)->addClause_(vec_lits) ? 1 : 0;
 }
